c0e0cdf2b67dfb3bd819927f2193ad8b9bb81713
evanofslack <51209817+evanofslack@users.noreply.github.com>
2022-02-10 22:25:10 -0500

    change auth middleware response error message

--- middleware/auth.go
+++ middleware/auth.go
@@ -14,7 +14,7 @@ func Authenticated(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
 		s, _ := session.Store.Get(r, session_name)
 		if auth, ok := s.Values["authenticated"].(bool); !auth || !ok {
-			http.Error(w, "Forbidden", http.StatusForbidden)
+			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
 
 		} else {
 			next.ServeHTTP(w, r)
